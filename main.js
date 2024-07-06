209fd81a1f0916bee6d642fc69ca91892674db07
Temiloluwa Gboyega <temiloluwagboyega@gmail.com>
2024-07-06 14:15:50 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// function myfunction(){
+//     var base = document.getElementById("pro")
+//     base.classList.toggle("change-pro")
+
+//     var base = document.getElementById("base")
+//     base.classList.toggle("")
+// }
